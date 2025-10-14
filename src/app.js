3b605c701e8b317ab5903bf220d6fc29cb1cccee
Leonardo Montesinos <leonardo.montesinos@utec.edu.pe>
2025-10-14 10:44:41 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,13 +2,18 @@ import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import routes from "./routes/booking.routes.js";
+import { mountSwagger } from "./docs/swagger.js"; 
 
 const app = express();
 app.use(helmet());
 app.use(express.json({ limit: "10mb" }));
 app.use(morgan("combined"));
 
+// Swagger antes del 404
+mountSwagger(app);
+
 app.use(routes);
+
 app.use((req, res) => res.status(404).json({ error: "Ruta no encontrada" }));
 
 export default app;
